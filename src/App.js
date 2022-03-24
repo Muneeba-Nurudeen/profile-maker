399daa68da51281b17b3cd1720852c6c43a3b453
Muneeba-Nurudeen <nurudeenmuneeba99@gmail.com>
2022-03-24 14:25:12 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React from "react";
+import ProfileForm from "./components/ProfileForm";
+import ProfileCard from "./components/ProfileCard";
+import { useState } from "react";
+
+function App() {
+  const [allProfiles, setAllProfiles] = useState([
+    {
+      firstname: "Ama",
+      lastname: "Mensah",
+      email: "mansah@gmail.com",
+      phone: "0276383996",
+    }
+  ]);
+  const updateProfiles = (profile) => {
+    let arr = allProfiles;
+    arr.push(profile);
+    setAllProfiles([...arr]);
+  };
+
+  return (
+    <div className="app">
+      <h1>Profile Maker</h1>
+      <div>
+        <ProfileForm submit={updateProfiles} />
+        <hr />
+        <div className="list">
+          {allProfiles.map((person, index) => (
+            <ProfileCard key={index} card={person} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+export default App;
